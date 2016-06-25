8bdc8a2f9fa114de990f8248d1545e5ce78cd9e6
zaargh <zaargh@protonmail.ch>
2016-06-25 16:20:53 +0200

    Grade school solved.

--- python/grade-school/school.py
+++ python/grade-school/school.py
@@ -3,11 +3,16 @@ import collections
 
 class School:
 
-    def __init__(self, school_name):
-        self.grades = collections.defaultdict(list)
+    def __init__(self, *args):
+        self.grades_dict = collections.defaultdict(list)
 
     def grade(self, grade_no):
-        return self.grades[grade_no]
+        return self.grades_dict[grade_no]
 
     def add(self, student, grade):
-        self.grades[grade].append(student)
+        self.grades_dict[grade].append(student)
+
+    def sort(self):
+        grades = sorted(k for k in self.grades_dict)
+        for g in grades:
+            yield g, tuple(sorted(self.grades_dict[g]))
