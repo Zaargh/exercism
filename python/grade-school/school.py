6848317740af19631763c9a25757d329c420b640
zaargh <zaargh@protonmail.ch>
2016-06-25 16:22:57 +0200

    added dunder to __grade_dict

--- python/grade-school/school.py
+++ python/grade-school/school.py
@@ -4,15 +4,15 @@ import collections
 class School:
 
     def __init__(self, *args):
-        self.grades_dict = collections.defaultdict(list)
+        self.__grades_dict = collections.defaultdict(list)
 
     def grade(self, grade_no):
-        return self.grades_dict[grade_no]
+        return self.__grades_dict[grade_no]
 
     def add(self, student, grade):
-        self.grades_dict[grade].append(student)
+        self.__grades_dict[grade].append(student)
 
     def sort(self):
-        grades = sorted(k for k in self.grades_dict)
+        grades = sorted(k for k in self.__grades_dict)
         for g in grades:
-            yield g, tuple(sorted(self.grades_dict[g]))
+            yield g, tuple(sorted(self.__grades_dict[g]))
